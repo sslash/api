d1f56bc3a9c0fb5ab671451d130da3412553482f
Michael Gunnulfsen <michaekg@ifi.uio.no>
2013-01-24 22:02:48 +0000

    sql

---
+++ sql/delete.sql
@@ -0,0 +1,11 @@
+DELETE FROM Fan;
+DELETE FROM ShredForBattle;
+DELETE FROM CommentForShred;
+DELETE FROM Rating;
+DELETE FROM TagsForShred;
+DELETE FROM Battle;
+DELETE FROM Shred;
+DELETE FROM GuitarForShredder;
+DELETE FROM EquiptmentForShredder;
+DELETE FROM Shredder;
+
--- sql/populateDB.pl
+++ sql/populateDB.pl
@@ -20,12 +20,12 @@ my @img =("EddieVanHalen.jpg", "RobertoDeMicheli.jpg", "michael.jpg",
 "richieH.jpg", "Thor.jpg", "slash.jpg", "MartSpart.jpg", "weegs.JPG",
 "MikeSpike.jpg", "YngwieMalmsteen.jpg", "Roberto.jpg" );
 
+
+
 my $date = strftime "%Y-%m-%d %H:%M:%S", localtime;
 
-
-
-
 open (MYFILE, '>>populateDB.sql');
+open (STUFF, '>>populateStuff.sql')
 open (SHREDS, '>>populateShreds.sql');
 open (BATTLES, '>>populateBattles.sql');
 open (FANS, '>>populateFans.sql');
@@ -35,10 +35,14 @@ open (SHREDDERS, '>>populateShredders.sql');
 
 # create 1000 shredders
 for (my $i = 0; $i < 1000; $i++) {
-    my $rand1 = int(rand(10));
-    my $rand2 = int(rand(10));
+    my $rand1 = int(rand(scalar @countries));
+    my $rand2 = int(rand(scalar @img));
     my $rand3 = int(rand(100));
     my $rand5 = int(rand(100));
+
+    my $randg = int(rand(scalar @g));
+    my $rande = int(rand(scalar @eq));
+    
     
     my $rand4 = int(rand(22));
     
@@ -46,34 +50,19 @@ for (my $i = 0; $i < 1000; $i++) {
     my $mail = "shredder". $i. "\@slash.com";
     my $description = "Simple test shredder #".$i;
     my $country = $countries[$rand1];
-    my $image = $img[$rand4];
+    my $image = $img[$rand2];
     
     
     my $SQL = "INSERT INTO Shredder values (DEFAULT, '$name', '$date', '$mail', '1111', '$description', '$country', DEFAULT, '$image', $rand3, $rand5);\n";
     my $shredder = "(SELECT Id FROM Shredder WHERE username='$name')";
     
-    my $guitar = "INSERT INTO GuitarForShredder VALUES ('$rand1', $shredder);\n";
-    my $equiptment = "INSERT INTO EquiptmentForShredder VALUES ('$rand2', $shredder);\n";
+    my $guitar = "INSERT INTO GuitarForShredder VALUES ('$randg', $shredder);\n";
+    my $equiptment = "INSERT INTO EquiptmentForShredder VALUES ('$rande', $shredder);\n";
     
-    print SHREDDERS "$SQL$guitar$equiptment";
+    print MYFILE "$SQL$guitar$equiptment";
 }
 
 
-
-
-
-
-my @shr = ( "23shred1.mp4", "battle-23-8-5.mp4", "fgdf.mp4", "23shred2.mp4",
-"battle-26-14-1.mp4", "g.mp4", "24shred5.mp4", "battle-27-10-1.mp4",
-"h.mp4", "9shred3.mp4", "battle-29-2-1.mp4", "legato.mp4",
-"9shred7.mp4", "battle-29-2-2.mp4", "livinprayer.mp4",
-"battle-17-6-2.mp4", "battle-29-6-1.mp4", "lovaabadname.mp4",
-"battle-17-7-2.mp4", "battle-29-6-2.mp4", "s.mp4",
-"battle-23-12-1.mp4", "battle-33-8-1.mp4", "sap sapsap2.mp4",
-"battle-23-12-2.mp4", "c.mp4", "sfhd.mp4", "battle-23-12-4.mp4",
-"crap.mp4", "sfsdf.mp4", "battle-23-12-5.mp4", "dfh.mp4", "sleep.mp4",
-"battle-23-12-6.mp4", "dminor.mp4", "vid.mp4", "battle-23-8-1.mp4",
-"edf.mp4", "ynsrv.mp4", "battle-23-8-3.mp4", "f.mp4" );
 
 my @tagsArr = ("Lick", "Fast", "Technique", "Sweeping", "Tapping", "Cover", "Solo",
 "Instruction", "Sound test", "Mind blowing", "Passionate", "British",
@@ -84,12 +73,30 @@ my @tagsArr = ("Lick", "Fast", "Technique", "Sweeping", "Tapping", "Cover", "Sol
 
 for ( my $i = 0; $i < scalar @tagsArr; $i++ ) {
     my $sql = "INSERT INTO Tag VALUES ( DEFAULT, '$tagsArr[$i]');";
-    print MYFILE "$sql\n";
+    #print STUFF "$sql\n";
 }
 
+my @shr = ("1.mp4", "13.mp4", "17.mp4", "20.mp4", "24.mp4", "26.mp4", "3.mp4",
+        "4.mp4", "8.mp4", "sapsapsap2.mp4", "sfsdf.mp4", "ynsrv.mp4", "10.mp4",
+        "14.mp4", "18.mp4", "21.mp4", "23shred1.mp4", "27.mp4", "30.mp4",
+        "5.mp4", "9.mp4", "9shred7.mp4", "crap.mp4", "dminor.mp4", "f.mp4",
+        "g.mp4", "legato.mp4", "lovaabadname.mp4", "11.mp4", "15.mp4",
+        "19.mp4", "22.mp4", "24shred5.mp4", "28.mp4", "31.mp4", "6.mp4",
+        "sfhd.mp4", "sleep.mp4", "vid.mp4", "12.mp4", "16.mp4", "2.mp4",
+        "23.mp4", "23shred2.mp4", "25.mp4", "29.mp4", "32.mp4", "7.mp4",
+        "9shred3.mp4", "c.mp4", "dfh.mp4", "edf.mp4", "fgdf.mp4", "h.mp4",
+        "livinprayer.mp4", "s.mp4");
+
+my @thumbs = ("23shred1.jpg", "9shred7.jpg", "crap.jpg", "dminor.jpg",
+        "f.jpg", "g.jpg", "legato.jpg", "lovaabadname.jpg", "test2.jpg",
+        "24shred5.jpg", "sfhd.jpg", "sleep.jpg", "vid.jpg", "23shred2.jpg",
+        "9shred3.jpg", "c.jpg", "dfh.jpg", "edf.jpg", "fgdf.jpg", "h.jpg",
+        "livinprayer.jpg", "s.jpg", "sfsdf.jpg", "test.jpg", "ynsrv.jpg");
+
 # Create 10000 shreds SELECT Id FROM Shredder ORDER BY RANDOM() LIMIT 1;
-for (my $i = 0; $i < 10000; $i++) {
-	 my $ran2 = int(rand(41));
+for (my $i = 0; $i < 50000; $i++) {
+    my $randTmb = int(rand(scalar @thumbs)); 
+	 my $ran2 = int(rand(scalar @shr));
      my $tagsRan1 = int(rand(scalar @tagsArr));
      my $tagsRan2 = int(rand(scalar @tagsArr));
     if ( $tagsRan1 == $tagsRan2) {
@@ -100,8 +107,7 @@ for (my $i = 0; $i < 10000; $i++) {
     my $commentText1 = "Lorem ipsum lol cat mode".$i;
     my $commentText2 = "Lorem ipsum lol cat mode".$i + 1;
     
-	my @imgs = split("\\.", $shr[$ran2]);
-    my $img = @imgs[0] . ".jpg";
+    my $img = $thumbs[$randTmb];
     my $description = "Simple test shred #" . $i; 
     my $owner = "SELECT Id FROM Shredder ORDER BY RANDOM() LIMIT 1";
     my $timeCreated = "DEFAULT";
@@ -109,7 +115,7 @@ for (my $i = 0; $i < 10000; $i++) {
     my $videoPath = $shr[$ran2];
     my $videoThumbnail = $img;
     my $sql = "INSERT INTO Shred VALUES(DEFAULT, '$description', ($owner),".
-        "$timeCreated, '$videoPath', '$shredType', 'battle-23-8-5.jpg');\n";
+        "$timeCreated, '$videoPath', '$shredType', '$img');\n";
 
     my $tagsSQL = "INSERT INTO TagsForShred VALUES(".
         "(SELECT currval('Shred_Id_seq')), ".
@@ -120,13 +126,13 @@ for (my $i = 0; $i < 10000; $i++) {
 
     my $commentowner = "SELECT Id FROM Shredder ORDER BY RANDOM() LIMIT 1";
     my $commentSQL = "INSERT INTO CommentForShred VALUES (".
-        "DEFAULT, '$commentText1', ($commentowner), DEFAULT, (SELECT currval('Shred_Id_seq')) );\n ";
+    "DEFAULT, '$commentText1', ($commentowner), DEFAULT, (SELECT currval('Shred_Id_seq')) );\n ";
     my $comment2SQL = "INSERT INTO CommentForShred VALUES (".
         "DEFAULT, '$commentText2', ($commentowner), DEFAULT, (SELECT currval('Shred_Id_seq')) );\n ";
 
     my $rating = "INSERT INTO Rating VALUES (".
         "(SELECT currval('Shred_Id_seq')), $ranrate2, $ranrate);\n";
-    print SHREDS $sql.$tagsSQL.$tagsSQL2.$commentSQL.$comment2SQL.$rating;
+    print MYFILE $sql.$tagsSQL.$tagsSQL2.$commentSQL.$comment2SQL.$rating;
 }
 
 # create battles
@@ -135,9 +141,10 @@ for ( my $i = 0; $i < 10000; $i++) {
     my $ran1 = int(rand($size));
     my $ran2 = int(rand($size));
     my $ran3 = int(rand(41));
-
-    my @imgs = split("\\.", $shr[$ran3]);
-    my $img = @imgs[0] . ".jpg";
+    my $ransh = int(rand(scalar @shr));
+    my $randTmb = int(rand(scalar @thumbs)); 
+    my $img = $thumbs[$randTmb];
+    my $sh =  $shr[$ransh];
     my $videoThumbnail = $img;
 
     my $battleSQL = "INSERT INTO Battle VALUES(".
@@ -154,16 +161,16 @@ for ( my $i = 0; $i < 10000; $i++) {
         "'',".
         "(SELECT Id FROM Shredder ORDER BY RANDOM() LIMIT 1),". # this is illegal, but I just need to have a shred owner
         "DEFAULT,".
-        "'$img',".
+        "'$sh',".
         "'battle',".
-        "'battle_50a508930364e84086ef7b7c_battle2.jpg');\n";
+        "'$img');\n";
 
     my $shredForBattleSQL = "INSERT INTO ShredForBattle VALUES(".
         "(SELECT currval('Shred_Id_seq')),".
         "(SELECT currval('Battle_Id_seq')),".
         "DEFAULT);\n";
 
-    print BATTLES $battleSQL.$shredSQL.$shredForBattleSQL;
+    print MYFILE $battleSQL.$shredSQL.$shredForBattleSQL;
 }
 
 
@@ -177,7 +184,7 @@ for ( my $i = 0; $i < 1000; $i++) {
             "(SELECT Id FROM Shredder ORDER BY RANDOM() LIMIT 1), ".
             "(SELECT Id FROM Shredder ORDER BY RANDOM() LIMIT 1), ".
             "DEFAULT);\n";
-        print FANS $fanSQL;
+        print MYFILE $fanSQL;
     }
 }
 
@@ -185,14 +192,15 @@ for ( my $i = 0; $i < 1000; $i++) {
 
 # create battle requests!
 my $size = 1000; # size of shredders! Add updated value here!
-for ( my $i = 0; $i < 10000; $i++) {
+for ( my $i = 0; $i < 1000; $i++) {
     my $ran1 = int(rand($size));
     my $ran2 = int(rand($size));
     my $ran3 = int(rand(41));
+    my $ransh = int(rand(scalar @shr));
+    my $randTmb = int(rand(scalar @thumbs)); 
+     my $img = $thumbs[$randTmb];
+    my $sh =  $shr[$ransh];
     
-    my @imgs = split("\\.", $shr[$ran3]);
-    my $img = @imgs[0] . ".jpg";
-    my $videoThumbnail = $img;
     
     my $battleSQL = "INSERT INTO Battle VALUES(".
     "DEFAULT,".
@@ -208,16 +216,16 @@ for ( my $i = 0; $i < 10000; $i++) {
     "'',".
     "(SELECT Id FROM Shredder ORDER BY RANDOM() LIMIT 1),". # this is illegal, but I just need to have a shred owner
     "DEFAULT,".
-    "'$img',".
+    "'$sh',".
     "'battle',".
-    "'battle_50a508930364e84086ef7b7c_battle2.jpg');\n";
+    "'$img');\n";
     
     my $shredForBattleSQL = "INSERT INTO ShredForBattle VALUES(".
     "(SELECT currval('Shred_Id_seq')),".
     "(SELECT currval('Battle_Id_seq')),".
     "DEFAULT);\n";
     
-    print BATTLEREQ $battleSQL.$shredSQL.$shredForBattleSQL;
+    print MYFILE $battleSQL.$shredSQL.$shredForBattleSQL;
 }
 
 
