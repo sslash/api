6ee94f0007f18ab45d1e7c35702ce927a736da6a
Michael Gunnulfsen <michaelgunnulfsen@gmail.com>
2013-03-20 19:00:57 +0100

    sap

---
+++ src/main/java/com/mikey/shredhub/api/service/exceptions/IllegalShredderArgumentException.java
@@ -0,0 +1,24 @@
+package com.mikey.shredhub.api.service.exceptions;
+
+public class IllegalShredderArgumentException extends Exception {
+
+	public IllegalShredderArgumentException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalShredderArgumentException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalShredderArgumentException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalShredderArgumentException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+}
